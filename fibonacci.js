70a868c37bf87f4aab85cc5d3aada49e259d64c8
NikithaNadig <nikitha.nadig@quintype.com>
2019-01-31 10:01:39 +0530

    added vowels and fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,19 @@
+
+
+function fib1(){
+    let t1=0,t2=1,next;
+    return fib;   
+    
+    function fib() {
+        next=t1+t2;
+        t1=t2;
+        t2=next;
+        return t1;
+    }
+    
+}
+
+
+
+run=fib1(); 
+
